725535952e1a48f0f137defb0a7f21f9451385e4
JackSparring420 <emanuele.arconte94@gmail.com>
2021-12-03 14:33:49 +0100

    add placeholder in search bar

--- src/components/Series.vue
+++ src/components/Series.vue
@@ -1,5 +1,6 @@
 <template>
-  <main>
+  <div v-if="series.length === 0"></div>
+  <main v-else>
     <h2>Serie TV</h2>
     <div id="series-container">
       <Serie v-for="serie, i in series" :key="i" :details="serie" />
@@ -23,13 +24,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
 main {
   background-color: #1e2d3b;
-}
-#series-container {
-    margin: auto;
-  display: flex;
-  flex-wrap: wrap;
-  padding: 50px;
+  
+  #series-container {
+      margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 50px;
+    }
+
+  h2 {
+    user-select: none;
   }
+}
 </style>
